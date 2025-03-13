5b774593f631d165079a7569aeca2d9f1b2ef145
蔡徐坤 <a48614897@163.com>
2025-03-13 13:35:02 +0800

    feat(components): 导出新组件并添加到全局组件库
    - 新增导出 Waterfall、Form、Table、Search、Dialog、Descriptions、SearchTable 和 Tips 组件
    - 在全局组件库中注册这些新组件
    - 注释掉 Drawer 组件的导出，因为该目录不存在

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,13 @@ export const setupGlobCom = (app: App<Element>): void => {
   app.component('Permission', Permission)
   app.component('BaseButton', BaseButton)
 }
+
+export * from './Waterfall'
+export * from './Form'
+export * from './Table'
+export * from './Search'
+export * from './Dialog'
+// export * from './Drawer' // 暂不存在此目录
+export * from './Descriptions'
+export * from './SearchTable'
+export * from './Tips'
