21453b02a2615d219f7d977f3032e61cda6cef69
蔡徐坤 <a48614897@163.com>
2025-03-13 13:33:33 +0800

    feat(components): 添加 Tips 组件
    - 创建 Tips 组件，用于显示带有图标的提示信息
    - 组件位于 src/components/Tips 目录下
    - 使用 Element Plus 的 ElTooltip 组件实现提示功能
    - 自定义图标使用项目中的 Icon 组件
    - 组件支持 content、icon 和 size 属性
    - 样式使用 scoped CSS 进行局部样式控制

---
+++ src/components/Tips/src/Tips.vue
@@ -0,0 +1,52 @@
+<template>
+  <span class="tips-wrapper">
+    <ElTooltip :content="content" placement="top" effect="light">
+      <span class="tips-icon">
+        <Icon :icon="icon" :size="Number(size)" />
+      </span>
+    </ElTooltip>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@/components/Icon'
+import { ElTooltip } from 'element-plus'
+
+defineProps({
+  /**
+   * 提示内容
+   */
+  content: {
+    type: String,
+    default: ''
+  },
+  /**
+   * 图标名称
+   */
+  icon: {
+    type: String,
+    default: 'vi-ep:question-filled'
+  },
+  /**
+   * 图标大小
+   */
+  size: {
+    type: [String, Number],
+    default: 12
+  }
+})
+</script>
+
+<style scoped>
+.tips-wrapper {
+  display: inline-flex;
+  align-items: center;
+  margin-left: 4px;
+  cursor: pointer;
+}
+
+.tips-icon {
+  display: inline-flex;
+  color: var(--el-color-info);
+}
+</style>
