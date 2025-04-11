645fbfa10c0480ba65a949ca88c349cd1f11d48f
蔡徐坤 <a48614897@163.com>
2025-04-11 22:51:29 +0800

    feat(ResourcePoolAccountApi): 新增资源池账户相关API接口
    - 在 src/api/system 目录下新增 resource_pool_account.ts 文件，定义了获取、创建、更新、删除及批量删除资源池账户的API接口，提升了系统的功能性和可扩展性。

---
+++ src/api/system/resource_pool_account.ts
@@ -0,0 +1,39 @@
+import request from '@/axios'
+
+/**
+ * 获取资源池账户列表
+ */
+export const getResourcePoolAccountListApi = (params: any) => {
+  return request.get({
+    url: '/manage/resource_pool/list', // 更新路径以匹配后端
+    params
+  })
+}
+
+/**
+ * 创建资源池账户
+ */
+export const createResourcePoolAccountApi = (data: any) => {
+  return request.post({ url: '/manage/resource_pool/add', data }) // 更新路径
+}
+
+/**
+ * 更新资源池账户
+ */
+export const updateResourcePoolAccountApi = (data: any) => {
+  return request.post({ url: '/manage/resource_pool/update', data }) // 更新路径和方法
+}
+
+/**
+ * 删除资源池账户
+ */
+export const deleteResourcePoolAccountApi = (params: any) => {
+  return request.post({ url: '/manage/resource_pool/delete', data: params }) // 更新路径和方法, 将 params 改为 data
+}
+
+/**
+ * 批量删除资源池账户
+ */
+export const batchDeleteResourcePoolAccountApi = (data: any) => {
+  return request.delete({ url: '/system/resource-pool-account/batch-delete', data })
+}
